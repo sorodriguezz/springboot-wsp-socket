b7c7b20d29a53d70573dc391393a860a7514f5b9
sorodriguezz <s.rodriguezzapata01@gmail.com>
2024-02-08 21:01:20 -0300

    first commit

---
+++ src/main/java/com/chatsocket/configuration/WebSocketConfiguration.java
@@ -0,0 +1,25 @@
+package com.chatsocket.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
+        messageBrokerRegistry.enableSimpleBroker("/topic");
+        messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/chat-socket")
+                .setAllowedOrigins("http://localhost:4200")
+                .withSockJS();
+
+    }
+}
